43d4d5da14b66c128126b7112a0d4d6710bb43b7
dotkalm <joelmholmberg@gmail.com>
2025-11-30 20:10:21 -0500

    logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,42 @@
 "use client";
 
 import * as React from 'react';
+import { useRouter } from 'next/navigation';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Image from 'next/image';
 
 export default function Header(){
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.replace('/');
+  };
+
   return (
     <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
       <Toolbar>
-        <Typography variant="h6">Chaos Agency</Typography>
+        <Box
+          onClick={handleLogoClick}
+          sx={{
+            cursor: 'pointer',
+            display: 'flex',
+            alignItems: 'center',
+            height: 40,
+            position: 'relative',
+            width: 'auto',
+          }}
+        >
+          <Image
+            src="/Chaos_Logotype.png"
+            alt="Chaos Agency"
+            width={150}
+            height={40}
+            priority
+            style={{ objectFit: 'contain' }}
+          />
+        </Box>
       </Toolbar>
     </AppBar>
   );
