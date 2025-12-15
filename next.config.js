e62f958f8c92ddd24c98ef4ad505ed36a891d5d9
dotkalm <joelmholmberg@gmail.com>
2025-12-15 17:15:55 -0500

    update basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || (isProd ? '/CHAOSWEBSITE' : '')
+
 const nextConfig = {
   output: 'export',
+  basePath: basePath,
+  assetPrefix: basePath,
   reactStrictMode: true,
   images: {
     unoptimized: true
