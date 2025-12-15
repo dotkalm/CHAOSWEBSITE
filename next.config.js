7c2b6972004db7a3fb29a770f3964b5eac187925
dotkalm <joelmholmberg@gmail.com>
2025-12-15 17:35:55 -0500

    rm static yml

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-// Only use basePath for GitHub Pages, not for Vercel
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true'
-const basePath = isGitHubPages ? '/CHAOSWEBSITE' : ''
-
 const nextConfig = {
   output: 'export',
-  basePath: basePath,
-  assetPrefix: basePath,
   reactStrictMode: true,
   images: {
     unoptimized: true
