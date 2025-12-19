087344e2ae9874985052c027040d89fceea548a6
dotkalm <joelmholmberg@gmail.com>
2025-12-19 14:08:34 -0500

    rm comment

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,16 +1,8 @@
 export const styles = {
     main: {
-                maxWidth: {
-                    xs: '100%',
-                    sm: '50vw',
-                }
-        /*
-        paddingRight: {
-            xs: 'inherit',
-            sm: '10vw',
-            md: '20vw',
-            lg: '40vw',
+        maxWidth: {
+            xs: '100%',
+            sm: '50vw',
         }
-        */
     }
 };
